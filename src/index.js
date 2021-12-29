8a1910cf50abd1533bc70617a4bf5ba144954604
Ahn-Gihwan <ahngh0113@naver.com>
2021-12-30 00:26:36 +0900

    style: import bootstrap css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import { worker } from "./mock/browser";
-
 import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 if (process.env.NODE_ENV === "development") worker.start();
 
