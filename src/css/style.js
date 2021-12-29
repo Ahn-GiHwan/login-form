31f060ee34bb483d2683e902896c2917fe9e2fd4
Ahn-Gihwan <ahngh0113@naver.com>
2021-12-30 01:04:09 +0900

    style: MenuIcon, input style

--- src/css/style.js
+++ src/css/style.js
@@ -1,7 +1,18 @@
 import styled from "styled-components";
 
-export const AppDiv = styled.div`
-  background-color: skyblue;
+export const MenuIcon = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: flex-end;
+  margin: 10px 10px 0 0;
+  font-size: 30px;
+`;
+
+export const Input = styled.input`
+  outline: none;
+  border: 0px solid;
+  border-bottom: 2px solid gray;
+  transition: 0.3s ease-in-out;
+  :focus {
+    transform: scale(1.1);
+  }
 `;
