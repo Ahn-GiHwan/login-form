91cb35e95e30af9f1c47a2df18681566176249a3
Ahn-Gihwan <ahngh0113@naver.com>
2022-01-02 18:42:38 +0900

    feat: Header&Navbar Component with reactstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,15 @@
+import { CloseOutlined, MenuOutlined } from "@ant-design/icons/lib/icons";
 import React, { useState } from "react";
-import { Button, Offcanvas, OffcanvasBody, OffcanvasHeader } from "reactstrap";
+import { NavLink } from "react-router-dom";
+import {
+  Button,
+  ListGroup,
+  ListGroupItem,
+  Offcanvas,
+  OffcanvasBody,
+  OffcanvasHeader,
+} from "reactstrap";
+import { MenuIcon } from "../css/style";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,13 +20,24 @@ const Header = () => {
 
   return (
     <div>
-      <Button color="primary" onClick={clickMenu}>
-        Open
-      </Button>
+      <MenuIcon>
+        {isOpen ? <CloseOutlined /> : <MenuOutlined onClick={clickMenu} />}
+      </MenuIcon>
       <Offcanvas isOpen={isOpen} toggle={clickMenu}>
-        <OffcanvasHeader toggle={clickMenu}>Offcanvas</OffcanvasHeader>
+        <OffcanvasHeader toggle={clickMenu}>메뉴</OffcanvasHeader>
         <OffcanvasBody>
-          <strong>This is the Offcanvas body.</strong>
+          <ListGroup flush>
+            <ListGroupItem>
+              <NavLink to="/login" onClick={clickMenu}>
+                로그인
+              </NavLink>
+            </ListGroupItem>
+            <ListGroupItem>
+              <NavLink to="/signup" onClick={clickMenu}>
+                회원가입
+              </NavLink>
+            </ListGroupItem>
+          </ListGroup>
         </OffcanvasBody>
       </Offcanvas>
     </div>
